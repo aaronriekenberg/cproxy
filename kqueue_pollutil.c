f438b716eaa71c8402e4685f1d2f7b14ae5340fc
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2012-10-13 15:16:55 -0500

    Update license.

--- kqueue_pollutil.c
+++ kqueue_pollutil.c
@@ -1,4 +1,4 @@
-/* cproxy - Copyright 2012 Aaron Riekenberg (aaron.riekenberg@gmail.com).
+/* cproxy - Copyright (C) 2012 Aaron Riekenberg (aaron.riekenberg@gmail.com).
 
    cproxy is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
